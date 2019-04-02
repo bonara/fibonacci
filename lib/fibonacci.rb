a1e9d3756ab961694227f800461be84678c6d7da
Nara <naraa_520@yahoo.com>
2019-04-01 21:58:38 -0700

    Added fibonacci method with passing tests.

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,8 +5,20 @@
 # ....
 # e.g. 6th fibonacci number is 8
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the nth fibonacci number.
+# Space complexity: O(1)
 def fibonacci(n)
-  raise NotImplementedError
+  raise ArgumentError.new("Invalid input of #{n}") if n == nil || n < 0
+  return 0 if n == 0
+  return 1 if n == 1
+  
+  first_num = 0
+  second_num = 1
+  while n > 1
+    value = first_num + second_num 
+    first_num = second_num
+    second_num = value
+    n -= 1
+  end
+  return value
 end
